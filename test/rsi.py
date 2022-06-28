dfac8f5eefce742443137d1ca2c5fdfb74ec612d
enicolasgomez <enicolasgomez@gmail.com>
2022-06-28 09:12:45 -0300

    Changed data provider

--- test/rsi.py
+++ test/rsi.py
@@ -46,7 +46,7 @@ def stochastic(data, k_window, d_window, window):
 
 def to_hourly_data(df, hours):
 
-  column_names = ["Date", "Open", "High", "Low", "Close", "Volume"]
+  column_names = ["Date", "Open", "High", "Low", "close", "Volume"]
   result_df = pd.DataFrame(columns = column_names)
 
   current_index = 0
@@ -69,7 +69,7 @@ def to_hourly_data(df, hours):
       total_vol = total_vol + row['Volume']
       current_index = current_index + 1
     else:
-      close = row['Close']
+      close = row['close']
       new_row = [date, open, current_high, current_low, close, total_vol]
       result_df = pd.concat([pd.DataFrame([new_row],columns=result_df.columns),result_df],ignore_index=True)
       open = 0
@@ -80,16 +80,22 @@ def to_hourly_data(df, hours):
   return result_df
 
 rsi_length = 14
-end_date = dt.datetime.today()
-start_date = end_date - dt.timedelta(days=950)
-stock = 'GOOG'
-#df = pdr.get_data_yahoo(stock, start_date, end_date)
-ticker = yf.Ticker(stock)
-df = ticker.history(interval="1D",start="2010-05-05",end="2022-05-18")
+# end_date = dt.datetime.today()
+# start_date = end_date - dt.timedelta(days=950)
+# stock = 'BTC-USD'
+# #df = pdr.get_data_yahoo(stock, start_date, end_date)
+# ticker = yf.Ticker(stock)
+# df = ticker.history(interval="1H",start="2021-05-01",end="2022-05-18")
 
+from Historic_Crypto import HistoricalData
+
+#df = HistoricalData('BTC-USD',3600,'2015-01-01-00-00').retrieve_data()
+#df.to_pickle("df.pkl")
+
+df = pd.read_pickle("df.pkl")
 #df = to_hourly_data(df, 4)
 
-df['RSI'] = computeRSI(df['Close'], rsi_length)
+df['RSI'] = computeRSI(df['close'], rsi_length)
 df['K'], df['D'] = stochastic(df['RSI'], 3, 3, rsi_length)
 
 def plot_srsi(k, d):
@@ -118,24 +124,43 @@ class Point:
 
 class Position:
   open = 0
-  close = 0
+  close_price = 0
   profit = 0
   type = 0
   date = None
   closeDate = None
+  stop_loss = 0
+  take_profit = 0
   def __init__(self, open, type, date):
     self.open = open 
     self.type = type
     self.date = date
-  def close(self, close, date):
-    self.close = close
+  def close(self, price, date):
+    self.close_price = price
     self.date = date
-    self.calculate_profit(close)
+    self.calculate_profit(price)
   def calculate_profit(self, close):
     if self.type == 'BUY':
-      self.profit = round(close - self.open)
+      self.profit = close - self.open
     else:
-      self.profit = round(self.open - close)
+      self.profit = self.open - close
+  def set_stop_loss(self, price):
+    self.stop_loss = price
+  def set_take_profit(self, price):
+    self.take_profit = price
+  def evaluate(self, price, date):
+    if self.type == 'BUY':
+      if price < self.stop_loss:
+        self.close(price, date)
+      if price > self.take_profit:
+        self.close(price, date)
+    elif self.type == 'SELL':
+      if price > self.stop_loss:
+        self.close(price, date)
+      if price < self.take_profit:
+        self.close(price, date)
+  def is_closed(self):
+    return self.close_price != 0 
 
 def AngleBtw2Points(pointA, pointB):
   changeInX = pointB.x - pointA.x
@@ -160,20 +185,22 @@ total_profit = 0
 signal_angle_enty = 0
 
 #angle in which K crossed D 
-signal_angle = 12
+signal_angle = 15
 #angle in which K crossed D, given an open position. If lower than this then closed. (tendency change)
-signal_angle_retire = 2
+signal_angle_retire = 15
 signal_bar_retire = 2
 current_signal_bar = 0
 
 #plot_srsi(df['K'], df['D'])
 profit = 0
 for index, row in df.iterrows(): 
+  close = row['close']
+  date = row['time']
   if index > rsi_length + 20 and last_row is not None:
     startK = Point(0, last_row['K']) #using 0 to 100 as SRSI is an oscillator (0, 100)
     startD = Point(0, last_row['D'])
-    endK = Point(100, row['K'])
-    endD = Point(100, row['D']) 
+    endK = Point(25, row['K'])
+    endD = Point(25, row['D']) 
     angle = AngleBtw2Points(startK, endK)
     if not openPosition:
       crossOver = CrossOver(startK, endK, startD) #K crosses D
@@ -182,26 +209,24 @@ for index, row in df.iterrows():
           signal_angle_entry = angle
           if angle > 0:
             signal_row = row
-            openPosition = Position(row['Close'], 'BUY', row['Date'])
+            openPosition = Position(close, 'BUY', date)
+            #openPosition.set_stop_loss(close * 0.5)
+            openPosition.set_take_profit(close * 1.05)
           else:
             signal_row = row
-            openPosition = Position(row['Close'], 'SELL', row['Date'])
+            openPosition = Position(close, 'SELL', date)
+            #openPosition.set_stop_loss(close * 1.5)
+            openPosition.set_take_profit(close * 0.95)
     else:
-      #evaluate
-      #startK = Point(0, last_row['K']) #using 0 to 100 as SRSI is an oscillator (0, 100)
-      #endK = Point(100, row['K'])
-      #angle = AngleBtw2Points(startK, endK)
-      #if current_signal_bar > signal_bar_retire:
-      if abs(angle) > signal_angle_entry - signal_angle_retire:
-        signal_bar_retire = 0
-        openPosition.close(row['Close'], row['Date'])
+      openPosition.evaluate(close, date)
+      if openPosition.is_closed():
         positions.append(openPosition)
-        profit_vector.append(total_profit)
         total_profit = total_profit + openPosition.profit
+        profit_vector.append(total_profit)
         openPosition = None 
         signal_row = None 
         openPosition = None 
-        signal_angle_entry = 0
-      current_signal_bar = current_signal_bar + 1
+
   last_row = row
 plot_profit(profit_vector)
+print()
